23fee3d82250e193b697f5128ebc5cd34cb0a3bc
KyleA <arbourkyle@gmail.com>
2022-03-22 16:52:26 -0400

    initial commit, starter files

--- tests/practice.test.js
+++ tests/practice.test.js
@@ -1,18 +1,15 @@
 const employee = require('../lib/employee.js')
 
 describe('make an employee profile', () =>{
-    expect(employee).toBe(employee);
+	test('name is name')
+	expect(name).toBe(name);
 
 }) 
 
-describe('make a name', () => {
-    expect(name).toBe(name)
+test('make an id', () => {
+	expect(id).toBe(id)
 })
 
-describe('make an id', () => {
-    expect(id).toBe(id)
-})
-
-describe('make an id', () => {
-    expect(id).toBe(id)
+test('make an email', () => {
+	expect(email).toBe(email)
 })
